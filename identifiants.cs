065f8fd4485976a59d32f4bd53ac2220c5056e8e
ProjetTutoreLP2018 <37624544+ProjetTutoreLP2018@users.noreply.github.com>
2018-04-24 15:00:22 +0200

    Add files via upload

---
+++ identifiants.cs
@@ -0,0 +1,169 @@
+Question: Quel est la raison social de votre entreprise ? : t8sY49V9KNrO
+Question: Sur quelle année d'exercice voulez-vous que nous intervenions ? : UvkGZwgvv4ew
+Question: Petite présentation du contact de votre entreprise : XHnKoFryrUed
+Question: Quel est son nom ? : Y83NJ0CQrBsq
+Question: Son petit prénom ? : Fiy3q2cF7KVk
+Question: Quel est sa fonction dans votre entreprise ? : m9RIpcje7Yox
+Question: Quel est le Chiffre d'Affaires de votre entreprise ? : g0WUh9KgPBBM
+Question: Quel est l'effectif de votre entreprise ? : FkDtid1fRnsF
+Question: Quel est votre organisation comptable ? : UZqEjku87mzV
+Question: Quel est votre volume de recette annuelle (Chiffre d'affaires, Subventions) ? : nXMR214HsT9T
+Question: Date d'immatriculation de votre entreprise : eJ0ffCVbctSs
+Question: Lieu d'immatriculation de votre entreprise : SFiOJCnku9bF
+
+
+
+
+landing_id =  0ab7561aa2a45726089ef070f650c088
+landing_id = 7ee06ad3c0e06636e576f53cba958bdb
+
+
+
+static string id_question_raison_sociale = "t8sY49V9KNrO";
+static string id_question_annee_exercice = "UvkGZwgvv4ew";
+static string id_question_presentation = "XHnKoFryrUed";
+static string id_question_nom = "Y83NJ0CQrBsq";
+static string id_question_prenom = "Fiy3q2cF7KVk";
+static string id_question_fonction = "m9RIpcje7Yox";
+static string id_question_ca = "g0WUh9KgPBBM";
+static string id_question_effectif = "FkDtid1fRnsF";
+static string id_question_organisation_comptable = "UZqEjku87mzV";
+static string id_question_volume_recette = "nXMR214HsT9T";
+static string id_question_date_immatriculation = "eJ0ffCVbctSs";
+static string id_question_lieu_immatriculation = "SFiOJCnku9bF";
+
+public class Presentation
+{
+	private string raison_sociale;
+	private string annee_exercice;
+	private string presentation;
+	private string nom;
+	private string prenom;
+	private string fonction;
+	private string ca;
+	private string effectif;
+	private string organisation_comptable;
+	private string volume_recette;
+	private string date_immatriculation;
+	private string lieu_immatriculation;
+	
+	public string getRaisonSociale()
+	{
+		return raison_sociale;
+	}
+	
+	public void setRaisonSociale(string raison_sociale)
+	{
+		this.raison_sociale = raison_sociale;
+	}
+	
+	public string getAnneeExercice()
+	{
+		return annee_exercice;
+	}
+	
+	public void setAnneeExercice(string annee_exercice)
+	{
+		this.annee_exercice = annee_exercice;
+	}
+	
+	public string getPresentation()
+	{
+		return presentation;
+	}
+	
+	public void setPresentation(string presentation)
+	{
+		this.presentation = presentation;
+	}
+	
+	public string getNom()
+	{
+		return nom;
+	}
+	
+	public void setNom(string nom)
+	{
+		this.nom = nom;
+	}
+	
+	public string getPrenom()
+	{
+		return prenom;
+	}
+	
+	public void setPrenom(string prenom)
+	{
+		this.prenom = prenom;
+	}
+	
+	public string getFonction()
+	{
+		return fonction;
+	}
+	
+	public void setFonction(string fonction)
+	{
+		this.fonction = fonction;
+	}
+	
+	public string getCa()
+	{
+		return ca;
+	}
+	
+	public void setCa(string ca)
+	{
+		this.ca = ca;
+	}
+	
+	public string getEffectif()
+	{
+		return effectif;
+	}
+	
+	public void setEffectif(string effectif)
+	{
+		this.effectif = effectif;
+	}
+	
+	public string getOrganisationComptable()
+	{
+		return organisation_comptable;
+	}
+	
+	public void setOrganisationComptable(string organisation_comptable)
+	{
+		this.organisation_comptable = organisation_comptable;
+	}
+	
+	public string getVolumeRecette()
+	{
+		return volume_recette;
+	}
+	
+	public void setVolumeRecette(string volume_recette)
+	{
+		this.volume_recette = volume_recette;
+	}
+	
+	public string getDateImmatriculation()
+	{
+		return date_immatriculation;
+	}
+	
+	public void setDateImmatriculation(string date_immatriculation)
+	{
+		this.date_immatriculation = date_immatriculation;
+	}
+	
+	public string getLieuImmatriculation()
+	{
+		return lieu_immatriculation;
+	}
+	
+	public void setLieuImmatriculation(string lieu_immatriculation)
+	{
+		this.lieu_immatriculation = lieu_immatriculation;
+	}
+}
